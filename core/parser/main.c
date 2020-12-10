c8bb12b9bd81e2cb583eca0c53ba0618d0e55983
Ilias Elbadaoui <iliassovich@outlook.com>
2020-12-10 19:45:25 +0100

    bus error/segfault solved

--- core/parser/main.c
+++ core/parser/main.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ielbadao <ielbadao@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: ielbadao <ielbadao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 14:12:46 by ielbadao          #+#    #+#             */
-/*   Updated: 2020/12/10 14:42:35 by ielbadao         ###   ########.fr       */
+/*   Updated: 2020/12/10 19:44:40 by ielbadao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
-
+#include <string.h>
+#include <stdio.h>
 int		main()
 {
 	// if (is_quote("\"ilias elbadaoui\""))
@@ -22,6 +23,7 @@ int		main()
 	// {
 	// 	out("IT'S NOT\n");
 	// }
-	trim("     ilias     \0");
+	
+	printf("|%s|",trim(strdup("     ilias     \0")));
 	return (0);
 }
--- core/parser/src/trim.c
+++ core/parser/src/trim.c
@@ -3,37 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   trim.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ielbadao <ielbadao@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: ielbadao <ielbadao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 13:04:52 by ielbadao          #+#    #+#             */
-/*   Updated: 2020/12/10 14:47:05 by ielbadao         ###   ########.fr       */
+/*   Updated: 2020/12/10 19:44:37 by ielbadao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../parser.h"
-#include <stdio.h>
 
 t_string		trim(t_string string)
 {
-	t_string	trimed;
 	int			start;
 	int			end;
 	
 	start = 0;
 	end = length(string);
-	if (!string || !(end - 1))
+	if (!string || !end)
 	 return (NULL);
 	while (string[start] == ' ' && string[start])
 		start++;
 	string += start;
-	out(string);
-	out("|\n");
-	end -= 1;
-	printf("AT END : %c|\n", string[end]);
+	end = length(string) - 1;
 	while (string[end] == ' ')
 		end--;
-	// string[end + 1] = '\0';
-	printf("END :%d\n", end);
-	trimed = NULL;
-	return (trimed);
+	string[end + 1] = '\0';
+	return (string);
 }
