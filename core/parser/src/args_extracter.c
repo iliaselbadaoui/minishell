2a28216f4f5e9813b6ec61c685c1718e10ecbb51
Ilias Elbadaoui <iliassovich@outlook.com>
2020-12-25 11:48:22 +0100

    FROM HOME TO SHCOOL

--- core/parser/src/args_extracter.c
+++ core/parser/src/args_extracter.c
@@ -6,10 +6,15 @@
 /*   By: ielbadao <ielbadao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/25 11:09:31 by ielbadao          #+#    #+#             */
-/*   Updated: 2020/12/25 11:09:48 by ielbadao         ###   ########.fr       */
+/*   Updated: 2020/12/25 11:48:17 by ielbadao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../parser.h"
 
+t_string		*args_extracter(t_string command)
+{
+	t_string	*args;
 
+	return (args);
+}
