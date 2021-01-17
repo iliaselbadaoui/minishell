53c76b7a0740b0c15e4bfc35fa6c1a5c6bf25f5a
Ilias Elbadaoui <iliassovich@outlook.com>
2021-01-18 00:33:44 +0100

    Some errors handled : split by the first delimiter and some other issues

--- main.c
+++ main.c
@@ -6,11 +6,7 @@
 /*   By: ielbadao <ielbadao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 14:12:46 by ielbadao          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/01/18 00:29:34 by ielbadao         ###   ########.fr       */
-=======
-/*   Updated: 2021/01/06 10:39:34 by ielbadao         ###   ########.fr       */
->>>>>>> 828092d177da354b40d7381cbafff2f609711962
+/*   Updated: 2021/01/18 00:33:19 by ielbadao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +30,7 @@ int		main(int argc, t_string *argv, t_string *envp)
 			if (syntax_checker(trim(line)))
 			{
 				list = parser(trim(line));
-<<<<<<< HEAD
 				// YOUR EXECUTION FUNCTION HERE LIKE => execute(list);
-=======
-				/*YOUR EXECUTION FUNCTION HERE*/
->>>>>>> 828092d177da354b40d7381cbafff2f609711962
 			}
 			free(line);
 			free_commands(&list);
