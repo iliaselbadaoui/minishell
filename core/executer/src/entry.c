625cea9c62bf4327138596ee1dfaed163f37c4f7
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2021-01-31 20:01:04 +0100

    .

--- core/executer/src/entry.c
+++ core/executer/src/entry.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   entry.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mait-si- <mait-si-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/31 19:54:14 by mait-si-          #+#    #+#             */
+/*   Updated: 2021/01/31 19:59:49 by mait-si-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../executer.h"
 
-t_bool is_cmd(t_string cmd)
+t_bool	is_cmd(t_string cmd)
 {
 	if (!ft_strcmp(cmd, "echo") ||
 		!ft_strcmp(cmd, "cd") ||
@@ -39,15 +51,19 @@ void	signal_handler(int signo)
 // 	}
 // }
 
-int execute(t_command *list)
+int		execute(t_command *list)
 {
-	if (!is_cmd(list->args[0]))
+	if (list)
 	{
-		out("minishell: ");
-		out(list->args[0]);
-		out(": command not found\n");
-		return (0);
+		if (!is_cmd(list->args[0]))
+		{
+			out("minishell: ");
+			out(list->args[0]);
+			out(": command not found\n");
+			return (0);
+		}
 	}
+
 	// print_struct(list);
 	return (1);
 }
