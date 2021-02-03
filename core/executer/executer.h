eea807625485f4a0cbeabf09e2ae1862bb571783
0x10000 <mohamedaitsimhand@gmail.com>
2021-02-03 10:13:04 +0100

    .

--- core/executer/executer.h
+++ core/executer/executer.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executer.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mait-si- <mait-si-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: 0x10000 <0x10000@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 13:06:00 by ielbadao          #+#    #+#             */
-/*   Updated: 2021/02/01 10:51:28 by mait-si-         ###   ########.fr       */
+/*   Updated: 2021/02/02 14:37:14 by 0x10000          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@
 int		exec_cmds(t_command *list);
 int		ft_strcmp(const char *s1, const char *s2);
 void	signal_handler(int signo);
+
+void	exit_shell(void);
 
 #endif
