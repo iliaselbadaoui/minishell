9055623fd337ce050a020f568da391046d453e7b
Ilias Elbadaoui <iliassovich@outlook.com>
2021-02-11 15:26:36 +0100

    FROM HOME TO SHCOOL: quotes removal is done and variable name extracter also done

--- core/parser/parser.h
+++ core/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: ielbadao <ielbadao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 11:47:41 by ielbadao          #+#    #+#             */
-/*   Updated: 2021/02/11 14:54:37 by ielbadao         ###   ########.fr       */
+/*   Updated: 2021/02/11 15:25:01 by ielbadao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -118,4 +118,6 @@ void			envp_reduce_size(void);
 void			add_to_envp(t_string key, t_string value);
 void			remove_from_envp(int index);
 void			remove_and_resize(int index);
+t_string		delete_quotes(t_string str);
+t_string		delete_dollar_mark(t_string str);
 #endif
---
+++ core/parser/src/delete_quotes.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   delete_quotes.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ielbadao <ielbadao@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/11 15:11:13 by ielbadao          #+#    #+#             */
+/*   Updated: 2021/02/11 15:25:08 by ielbadao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../parser.h"
+
+t_string		delete_quotes(t_string str)
+{
+	t_string no_quote;
+
+	no_quote = substring(str, 1, ft_strlen(str) - 2);
+	return (no_quote);
+}
+
+t_string		delete_dollar_mark(t_string str)
+{
+	t_string no_dollar;
+
+	no_dollar = substring(str, 1, ft_strlen(str) - 1);
+	return (no_dollar);
+}
