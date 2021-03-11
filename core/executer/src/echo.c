22877d8feff2b239904b00c62c87e0485c08fadf
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2021-03-11 11:16:05 +0100

    .

--- core/executer/src/echo.c
+++ core/executer/src/echo.c
@@ -6,7 +6,7 @@
 /*   By: mait-si- <mait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 18:55:26 by mait-si-          #+#    #+#             */
-/*   Updated: 2021/03/10 18:25:44 by mait-si-         ###   ########.fr       */
+/*   Updated: 2021/03/11 11:07:21 by mait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,28 +104,25 @@ int				echo(t_string *args, int fd)
 	j = -1;
 	while (args[++i])
 	{
-		// out("[");
-		// out(args[i]);
-		// out("]\n");
+		out("[");
+		out(args[i]);
+		out("]\n");
 		j = 1;
-		if (args[i][0] == '-' && args[i][1] == 'n')
-		{
-			n_option++;
-			while (args[i][++j] == 'n');
-			if (args[i][j] != '\0')
-				n_option = 0;
-		}
-		else
-		{
-			j = n_option ? 0 : -1;
+		// if (args[i][0] == '-' && args[i][1] == 'n')
+		// {
+		// 	n_option++;
+		// 	while (args[i][++j] == 'n');
+		// 	if (args[i][j] != '\0')
+		// 		n_option = 0;
+		// }
+		// else
+		// {
+		// 	j = n_option ? 0 : -1;
 			// put_argument(args[i], fd, j);
-		}
-		if (n_option)
-			continue ;
-		j = n_option ? 0 : -1;
-
-		if (n_option)
-			out();
+		// }
+		// if (n_option)
+		// 	continue ;
+		// j = n_option ? 0 : -1;
 	}
 	if (!n_option)
 		write(fd, "\n", 1);
