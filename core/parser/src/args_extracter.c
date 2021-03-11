3534376e816af670ce1f58e6a8101e62f03d1e4d
Ilias Elbadaoui <iliassovich@outlook.com>
2021-03-11 14:23:02 +0100

    FROM HOME TO SHCOOL: no more spaces

--- core/parser/src/args_extracter.c
+++ core/parser/src/args_extracter.c
@@ -6,13 +6,13 @@
 /*   By: ielbadao <ielbadao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/25 11:09:31 by ielbadao          #+#    #+#             */
-/*   Updated: 2021/03/11 11:14:42 by ielbadao         ###   ########.fr       */
+/*   Updated: 2021/03/11 14:21:32 by ielbadao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../parser.h"
 
-static void		extracter_helper(t_string *args, t_string command, int *first, int i)
+static void		extracter_helper(t_string *args, t_string command, int i)
 {
 	t_coord		coord;
 	t_string	tmp;
@@ -22,22 +22,8 @@ static void		extracter_helper(t_string *args, t_string command, int *first, int 
 	if (coord.end >= coord.start)
 	{
 		tmp = substring(command, coord.start, coord.end);
-		if (!(*first) && !is_option(trim(tmp)) && i != 0)
-		{
-			args[i] = ft_strdup(trim(tmp));
-			*first = 1;
-			free(tmp);
-		}
-		else
-		{
-			args[i] = ft_strdup(tmp);
-			free(tmp);
-			if (!(tmp = args[i]) && is_option(trim(args[i])))
-			{
-				args[i] = ft_strdup(trim(args[i]));
-				free(tmp);
-			}
-		}
+		args[i] = ft_strdup(trim(tmp));
+		free(tmp);
 	}
 }
 
@@ -46,17 +32,15 @@ t_string		*args_extracter(t_string command)
 	t_string	*args;
 	int			count;
 	int			i;
-	int			first;
 
 	g_counter = 0;
-	first = 0;
 	count = args_calculator(command) + 1;
 	g_counter = 0;
 	args = (t_string *)malloc(sizeof(t_string) * count);
 	i = 0;
 	while (i < count - 1)
 	{
-		extracter_helper(args, command, &first, i);
+		extracter_helper(args, command, i);
 		i++;
 	}
 	args[i] = NULL;
