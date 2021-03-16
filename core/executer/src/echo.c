23b63992908ceb0ec745db4f874c2bb44e4a4d7c
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2021-03-16 13:01:12 +0100

    Echo with option -n is ready

--- core/executer/src/echo.c
+++ core/executer/src/echo.c
@@ -6,7 +6,7 @@
 /*   By: mait-si- <mait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 18:55:26 by mait-si-          #+#    #+#             */
-/*   Updated: 2021/03/15 11:43:59 by mait-si-         ###   ########.fr       */
+/*   Updated: 2021/03/16 13:00:05 by mait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,23 +31,24 @@ int				echo(t_string *args, int fd)
 {
 	int			i;
 	int			j;
-	t_bool		n_option;
+	int			n_option;
 	t_string	str;
 
-	n_option = false;
+	n_option = 0;
 	i = 0;
 	j = -1;
 	while (args[++i])
-		if (is_option_n(args[i]))
-			n_option = true;
+		if (is_option_n(filter(args[i])))
+			n_option++;
 		else
 			break ;
 	while (args[i])
 	{
 		str = filter(args[i++]);
-		write(fd, str, ft_strlen(str));
-		if (*str != '\0')
+		if (*str != '\0' && n_option < i - 2)
 			write(fd, " ", 1);
+		write(fd, str, ft_strlen(str));
+		// add space in the begining it the argument is not the first, and is not an empty string
 	}
 	if (!n_option)
 		write(fd, "\n", 1);
