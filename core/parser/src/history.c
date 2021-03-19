6dad093d5aac54433bbe008e4143b6ce08e20b1e
iliaselbadaoui <iliassovich@outlook.com>
2021-03-19 15:00:34 +0100

    NORMINETTE DONE

--- core/parser/src/history.c
+++ core/parser/src/history.c
@@ -6,7 +6,7 @@
 /*   By: ielbadao <ielbadao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 09:55:01 by ielbadao          #+#    #+#             */
-/*   Updated: 2021/03/19 10:44:55 by ielbadao         ###   ########.fr       */
+/*   Updated: 2021/03/19 14:59:32 by ielbadao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 
 int				history(void)
 {
-	tgetent(g_term_buffer, getenv("TERM"));
-	return (0);
+	t_string	termtype;
+  	int			success;
+
+	termtype = getenv("TERM");
+	if (termtype == 0)
+		out("Specify a terminal type with `setenv TERM <yourtype>'.\n");
+
+	success = tgetent (g_term_buffer, termtype);
+	if (success < 0)
+		out("Could not access the termcap data base.\n");
+	if (success == 0)
+	{
+		out("Terminal type `");
+		out(termtype);
+		out("' is not defined.\n");
+	}
 }
